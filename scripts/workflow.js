6742939f825f79b2ac69cbd2f7db9c2daaf3ebf2
yarpenart <karolass11@gmail.com>
2026-07-19 20:06:35 +0200

    update to v0.2.14

--- scripts/workflow.js
+++ scripts/workflow.js
@@ -438,7 +438,7 @@ export function initializeWorkflow() {
 
   game.counterspellPlus = {
     startFromActivity: startCounterspell,
-    version: "0.2.13"
+    version: "0.2.14"
   };
 
   debug("Ready");
